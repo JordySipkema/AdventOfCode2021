2f5bd89b843d207cdbd8aefa60125b5eb340c23f
Jordy Sipkema <jordysipkema@gmail.com>
2021-12-20 10:16:00 +0100

    Add comments

--- Day1-FSharp/Program.fs
+++ Day1-FSharp/Program.fs
@@ -1,8 +1,10 @@
 ﻿namespace AdventOfCode2021
 
 module Day01 = 
+    open System
     open System.IO
 
+    // Shared
     let inputFile = "./input/input.txt"
 
     let readInput =
@@ -14,29 +16,29 @@ module Day01 =
     let countHigherThanlast inputList =
         inputList
         |> List.pairwise     // Collect items per-pair
-        |> List.choose(fun (num1,num2) -> if num2 > num1 then Some 1 else None)
-        |> List.reduce(+)
+        |> List.choose(fun (num1,num2) -> if num2 > num1 then Some 1 else None) 
+                             // return 1 if higher, discard all others
+        |> List.reduce(+)    // Sum all the '1's
 
     let solutionPart1 =
         readInput
         |> countHigherThanlast
         |> sprintf "The answer is: %A"
-        |> System.Console.WriteLine
+        |> Console.WriteLine
 
     // Part Two
-
     let windowedSum windowSize list =
         list
-        |> List.windowed windowSize
-        |> List.map (List.reduce(+))
+        |> List.windowed windowSize  // Use native F# List.windowed func to gather all the windows.
+        |> List.map (List.reduce(+)) // Count the sum of all windows and return them as a list.
 
     let solutionPart2 =
         readInput
         |> windowedSum 3
         |> countHigherThanlast
         |> sprintf "The answer is: %A"
-        |> System.Console.WriteLine
+        |> Console.WriteLine
 
-
+    // Execute functions to display results
     solutionPart1 |> ignore
     solutionPart2 |> ignore
